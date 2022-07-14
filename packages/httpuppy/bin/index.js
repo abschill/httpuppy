6903f5c41b6e085fdaf2e1c34565f9746ba354ca
abschill <ajay198810@gmail.com>
2022-07-14 14:00:59 -0600

    :bug: cli logger@0.4.5

--- packages/httpuppy/bin/index.js
+++ packages/httpuppy/bin/index.js
@@ -1,10 +1,9 @@
 #! /usr/bin/env node
 const { color_tag } = require('../lib/internal/include');
 const { useServer } = require('../lib');
-const cluster = require('cluster');
-const { resolve } = require('path');
 const { log } = console;
 const args = process.argv;
+
 
 if(!args.includes('--serve')) {
 	log(color_tag('red', 'error: must provide a directory with the --serve option'));
@@ -18,20 +17,12 @@ if(args.length <= serve_index) {
 	log(color_tag('red', 'error: no directory provided after --serve'));
 	process.exit(1);
 }
-
 const hot_dir = args[args.indexOf('--serve')+1];
-if(cluster.isPrimary) {
-	const serve_dir = resolve(process.cwd(), hot_dir);
-	log(color_tag('blue', '[httpuppy]'), color_tag('green', 'server started'));
-	log(color_tag('green', '\nServer Options:\n'));
-
-	log(color_tag('purp', 'port'), ':', port);
-	log(color_tag('purp', 'static directory:'), serve_dir);
-}
 
 const server = useServer({
 	clustered: true,
-	port
+	port,
+	log_level: 'verbose'
 });
 
 server.static('/', hot_dir);
