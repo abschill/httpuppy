fc30202237c21632027cd7e42b8919db5e309098
abschill <ajay198810@gmail.com>
2022-07-15 12:01:21 -0600

    :technologist: cli updates@0.4.6

--- packages/httpuppy/bin/index.js
+++ packages/httpuppy/bin/index.js
@@ -1,9 +1,9 @@
 #! /usr/bin/env node
-const { color_tag } = require('../lib/internal/include');
+const { color } = require('terminal-color');
 const { useServer } = require('../lib');
+const { resolve } = require('path');
 const { log } = console;
 const args = process.argv;
-
 
 if(!args.includes('--serve')) {
 	log(color_tag('red', 'error: must provide a directory with the --serve option'));
@@ -20,10 +20,18 @@ if(args.length <= serve_index) {
 const hot_dir = args[args.indexOf('--serve')+1];
 
 const server = useServer({
-	clustered: true,
+	clustered: false,
 	port,
 	log_level: 'verbose'
 });
 
 server.static('/', hot_dir);
 server.start();
+
+log(`
+${color.fg.green('Server Listening')}
+${color.fg.yellow('Port:')} ${color.fg.purple(port)}
+${color.fg.yellow('Directory:')} ${resolve(process.cwd(), hot_dir)}
+${color.fg.yellow('Base View:')} ${server._vfs.mountedFiles.filter(f => f.hrefs.includes('/'))[0].symLink}
+${color.fg.blue('Host:')} ${color.fg.yellow(server.pConfig.hostname ?? 'http://127.0.0.1:')}${color.fg.purple(port)}
+`);
