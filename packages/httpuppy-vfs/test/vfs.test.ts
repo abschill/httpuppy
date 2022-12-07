998e505a0f82f6b7e3da50be4b42fa2a71a9d807
abschill <ajay198810@gmail.com>
2022-12-06 18:06:27 -0700

    test runner for vfs

--- packages/httpuppy-vfs/test/vfs.test.ts
+++ packages/httpuppy-vfs/test/vfs.test.ts
@@ -1,16 +1,16 @@
 import expect from 'expect';
 import { it, describe } from 'mocha';
-import { create_nested_vfs, local_mime_type } from '../lib';
+import { $useVfs, $vMimeType } from '../lib';
 import { resolve } from 'path';
 
 describe('base tests', () => {
-    it('Gets Proper MIME Type for HTML', () => {
-        const mime_html = local_mime_type(resolve(process.cwd(), '__fixtures/index.html'));
-        return expect(mime_html).toMatch(/html/g);
-    });
-    it('Gets 2 vfs files in a dir of 2 files with a subdir', async () => {
-        const vfs_test = await create_nested_vfs('/', resolve(process.cwd(), '__fixtures__'));
-        expect(vfs_test).toHaveProperty('mounted_files');
-        expect(vfs_test.mounted_files).toHaveLength(2);
-    });
+	it('Gets Proper MIME Type for HTML', () => {
+		const mime_html = $vMimeType(resolve(process.cwd(), '__fixtures/index.html'));
+		return expect(mime_html).toMatch(/html/g);
+	});
+	it('Gets 2 vfs files in a dir of 2 files with a subdir', async () => {
+		const vfs_test = await $useVfs('/', resolve(process.cwd(), '__fixtures__'));
+		expect(vfs_test).toHaveProperty('mounted_files');
+		expect(vfs_test.mounted_files).toHaveLength(2);
+	});
 });
